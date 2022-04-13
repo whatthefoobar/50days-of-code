d2000986bf6d9d939da28b0d05ade482498dd6a0
whatthefoobar <irina.mntn@gmail.com>
2022-04-13 16:50:39 +0200

    project 5 and 6 done

---
+++ 6. 3D rotating nav menu/app.js
@@ -0,0 +1,13 @@
+// sticky nav bar
+const nav = document.querySelector('nav');
+let navTop = nav.offsetTop;
+
+function fixedNav() {
+  if (window.scrollY >= navTop) {
+    nav.classList.add('fixed');
+  } else {
+    nav.classList.remove('fixed');
+  }
+}
+
+window.addEventListener('scroll', fixedNav);
