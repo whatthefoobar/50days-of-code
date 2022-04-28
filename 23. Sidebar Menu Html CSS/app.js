985025cd3131e08ce791be782c5caa960d776134
whatthefoobar <irina.mntn@gmail.com>
2022-04-28 20:44:10 +0200

    fixed missing proj 12

--- 23. Sidebar Menu Html CSS/app.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.querySelector('nav');
-const toggle = document.querySelectorAll('.toggle');
-
-for (let i = 0; i <= toggle.length; i++) {
-  toggle[i].addEventListener('click', function () {
-    sidebar.classList.toggle('active');
-  });
-}
