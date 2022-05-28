8b0d4a49c704e927d3ce72d5aeead2b36909a921
whatthefoobar <irina.mntn@gmail.com>
2022-05-28 15:03:31 +0200

    proj 25 done and boilerplate for 26

---
+++ 25. Live User Filter/app.js
@@ -0,0 +1,40 @@
+const result = document.querySelector('#result');
+const filter = document.querySelector('#filter');
+const listItems = [];
+
+async function getData() {
+  const res = await fetch('https://randomuser.me/api?results=50');
+  const { results } = await res.json();
+
+  // Clear results
+  results.innerHTML = '';
+  results.forEach((user) => {
+    const li = document.createElement('li');
+    listItems.push(li);
+    li.innerHTML = `
+          <img
+            src=${user.picture.large}
+            alt=${user.name.first}
+          />
+          <div class="user-info">
+            <h4>${user.name.first} ${user.name.last}</h4>
+            <p>${user.location.city}, ${user.location.country}</p>
+          </div>
+        `;
+    result.appendChild(li);
+  });
+}
+
+getData();
+
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove('hide');
+    } else {
+      item.classList.add('hide');
+    }
+  });
+}
+
+filter.addEventListener('input', (e) => filterData(e.target.value));
