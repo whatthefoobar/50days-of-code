39d15820ea7509a2833f70fde003ac0de1716530
whatthefoobar <irina.mntn@gmail.com>
2022-06-07 08:44:20 +0200

    done

---
+++ 29. File Downloader/app.js
@@ -0,0 +1,28 @@
+const fileInput = document.querySelector('input'),
+  downloadBtn = document.querySelector('button');
+downloadBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  downloadBtn.innerText = 'Downloading file...';
+  fetchFile(fileInput.value);
+});
+
+function fetchFile(url) {
+  fetch(url)
+    .then((res) => res.blob())
+    .then((file) => {
+      //URL.createObjectURL() creates an url of passed object
+      let tempUrl = URL.createObjectURL(file);
+      const aTag = document.createElement('a');
+      aTag.href = tempUrl;
+      aTag.download = url.replace(/^.*[\\\/]/, '');
+      document.body.appendChild(aTag);
+      aTag.click();
+      downloadBtn.innerText = 'Download File';
+      URL.revokeObjectURL(tempUrl);
+      aTag.remove();
+    })
+    .catch(() => {
+      alert('Failed to download file!');
+      downloadBtn.innerText = 'Download File';
+    });
+}
